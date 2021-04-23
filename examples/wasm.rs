2f887adc7c088e17cf784982205b5ac6dffd3c58
Mitchell Keith Bloch <bazald@gmail.com>
2021-04-22 20:04:28 -0700

    Add old parallel work in progress

--- examples/wasm.rs
+++ examples/wasm.rs
@@ -1,21 +1,35 @@
 extern crate tokio;
-extern crate warp;
+#[macro_use] extern crate warp;
 
-use warp::Filter;
+use tokio_compat_02::FutureExt;
+use warp::{Filter, http::header::{HeaderMap, HeaderValue}};
 
 #[tokio::main]
 async fn main() {
     std::process::Command::new("wasm-pack")
-        .args(&["build", "--target", "web"])
+        .args(&["build", "--dev", "--target", "web"])
         .output()
         .expect("wasm-pack failed -- try `cargo install wasm-pack`");
 
-    let routes = warp::get().and(warp::path::end()).and(warp::fs::file("examples/wasm.html"))
-        .or(warp::fs::dir("pkg"));
+    let mut headers = HeaderMap::new();
+    headers.insert("Cross-Origin-Opener-Policy", HeaderValue::from_static("same-origin"));
+    headers.insert("Cross-Origin-Embedder-Policy", HeaderValue::from_static("require-corp"));
+    
+    let routes = warp::get().and(warp::path::end())
+            .and(warp::fs::file("examples/wasm.html"))
+        .or(warp::get().and(path!("worker.js")).and(warp::path::end())
+            .and(warp::fs::file("examples/worker.js").with(warp::reply::with::headers(headers.clone()))))
+        .or(warp::fs::dir("pkg").with(warp::reply::with::headers(headers)));
 
-    let serve_result = warp::serve(routes).run(([127, 0, 0, 1], 4353));
+    let serve_result = warp::serve(routes)
+        .tls()
+        .cert_path("examples/cert.pem")
+        .key_path("examples/key.rsa")
+        .run(([127, 0, 0, 1], 4353));
 
-    println!("Check out this wasm example at https://127.0.0.1:4353");
+    println!("Check out this wasm example at http://127.0.0.1:4353");
 
-    serve_result.await;
+    serve_result
+        .compat()
+        .await;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,13 @@ mod map_reduce_set;
 mod node_id;
 mod parallelization;
 mod symbol;
+#[macro_use] mod wasm_parallel;
 mod wme;
 
+use futures_channel::oneshot;
+use js_sys::Promise;
+use rayon::prelude::*;
+use std::future::Future;
 use std::sync::atomic::Ordering;
 use wasm_bindgen::prelude::*;
 use xarc::AtomicXarc;
@@ -22,8 +27,48 @@ extern {
 }
 
 #[wasm_bindgen]
+pub fn rete_init() {
+    console_error_panic_hook::set_once();
+}
+
+#[wasm_bindgen]
 pub fn greet(name: &str) {
-    let x = AtomicXarc::new(42);
-    let y = x.load(Ordering::Relaxed);
     alert(&format!("Hello, {}!", name));
+    console_log!("Hello, log!");
+}
+
+#[wasm_bindgen]
+pub fn par_sum(
+    concurrency: usize,
+    thread_pool: &wasm_parallel::pool::WorkerPool,
+    data: Vec<i32>,
+) -> Result<Promise, JsValue> {
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(concurrency)
+        .spawn_handler(|thread| Ok(thread_pool.run(|| thread.run()).unwrap()))
+        .build()
+        .unwrap();
+
+    // And now execute the render! The entire render happens on our worker
+    // threads so we don't lock up the main thread, so we ship off a thread
+    // which actually does the whole rayon business. When our returned
+    // future is resolved we can pull out the final version of the image.
+    let (tx, rx) = oneshot::channel();
+    let mut sum = 0;
+    thread_pool.run(move || {
+        pool.install(|| {
+            sum = data
+                .par_chunks(32)
+                .map(|x| {let mut r = 0; for y in x {r += y;} r})
+                .reduce(|| 0, |x, y| x + y);
+        });
+        let _ = tx.send(sum);
+    })?;
+
+    Ok(wasm_bindgen_futures::future_to_promise(async move {
+        match rx.await {
+            Ok(_data) => Ok(JsValue::from(_data)),
+            Err(_) => Err(JsValue::undefined()),
+        }
+    }))
 }
